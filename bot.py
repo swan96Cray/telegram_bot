955708f96c18b224435b9339321d186c0e458a08
swan96Cray <swan45kurgan@gmail.com>
2026-04-06 22:41:04 +0500

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,17 @@
+import telebot
+import os
+
+TOKEN = os.environ.get("TOKEN")
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "✅ Бот работает на Render!")
+
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    bot.send_message(message.chat.id, "Ты написал: " + message.text)
+
+print("🚀 Бот запущен...")
+bot.infinity_polling()
